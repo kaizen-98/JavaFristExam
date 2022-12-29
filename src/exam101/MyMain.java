61090c63ccbef55a44baf691ce5cae5da5aa0dfe
kaizen-98 <ammalnassri@gmail.com>
2022-12-28 23:09:10 -0800

    frist move

--- src/exam101/MyMain.java
+++ src/exam101/MyMain.java
@@ -1,10 +1,23 @@
 package exam101;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MyMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		BufferedWriter bW = new BufferedWriter(new FileWriter("\\Users\\Lenovo\\eclipseee\\projectExam101\\files101\\file101.pdf"));
+		bW.write("java html python css javascript");
+		bW.write("\njava html python css javascript");
+		bW.close();
+		
+		
+		
+		
 	}
 
 }

2652580ac5d541e6f790601be43e21998f8789b8
kaizen-98 <ammalnassri@gmail.com>
2022-12-29 04:09:35 -0800

    update and add user input

--- src/exam101/MyMain.java
+++ src/exam101/MyMain.java
@@ -3,8 +3,10 @@ package exam101;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -26,28 +28,45 @@ public class MyMain {
 		Scanner scan = new Scanner(file);
 		String txtContent ="";
 		
+	//	FilenameFilter textFilter = new FilenameFilter() {
+		//	public boolean accept(File file, String name) {
+		//	return name.toLowerCase().endsWith(".pdf");
+		//	}
+		//	};
+			
+			
 			while(scan.hasNextLine()) {
 				
 				//System.out.println(scan.nextLine());
 				txtContent = txtContent.concat(scan.nextLine() + "\n");
-			
-			
-			
-		}
+			}
 		for (int i =0; i<99; i++) {
-			 write = new FileWriter("\\Users\\Lenovo\\eclipseee\\projectExam101\\file101"+i+".pdf");
-			 write.write(txtContent);
-			 write.close();
+			BufferedWriter br = new BufferedWriter( new FileWriter("\\Users\\Lenovo\\eclipseee\\projectExam101\\file101"+i+".pdf"));
+			 br.write(txtContent);
+			 br.close();
 				
 		}
+		BufferedReader brr = new BufferedReader(new FileReader("\\Users\\Lenovo\\eclipseee\\projectExam101\\file101.pdf"));
 		
-		System.out.println("write word wannna seach about?");
-		String word = scan.next();
-			while(scan.equals(word)) {
-				System.out.println(scan.nextLine());
-			
-			
+		Scanner sc = new Scanner(System.in);
+		int wordCount = 0;
+		String s;
+		System.out.println("Enter the word you want to search pdf ");
+		String input = sc.next();
+		while((s=brr.readLine())!=null) {
+			if(s.equals(input)) {
+				wordCount++;
+			}
 		}
-	}
+		
+		if(wordCount!=0) {
+			System.out.println("Number of word here that you seach "+wordCount);
+		}
+		else {
+			System.out.println("We couldnt find it");
+		}
+	
+		
+	
 
-}
+}}
